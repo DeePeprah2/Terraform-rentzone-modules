46cc7ea5b7b334e33ab98591dac5268dbd1626d9
Deepeprah <deebio2@yahoo.co.uk>
2023-03-24 23:55:33 +0000

    elearning project

--- vpc/output.tf
+++ vpc/output.tf
@@ -1,19 +1,19 @@
-# export output region 
+// export output region 
 output "region" {
   value = var.region
 }
 
-# export environment
+// export environment
 output "environment" {
   value = var.environment
 }
 
-# export project name
+// export project name
 output "project_name" {
   value = var.project_name
 }
 
-#export vpc output
+ // export vpc output
 output "vpc_id" {
   value = aws_vpc.elearning-vpc.id
 }
@@ -22,37 +22,37 @@ output "public_subnet_az1_id" {
   value = aws_subnet.public_subnet_az1.id
 }
 
-# export the public subnet az2 id
+// export the public subnet az2 id
 output "public_subnet_az2_id" {
   value = aws_subnet.public_subnet_az2.id
 }
 
-# export the private app subnet az1 id
+// export the private app subnet az1 id
 output "private_app_subnet_az1_id" {
   value = aws_subnet.private_app_subnet_az1.id
 }
 
-# export the private app subnet az2 id
+// export the private app subnet az2 id
 output "private_app_subnet_az2_id" {
   value = aws_subnet.private_app_subnet_az2.id
 }
 
-# export the private data subnet az1 id
+// export the private data subnet az1 id
 output "private_data_subnet_az1_id" {
   value = aws_subnet.private_data_subnet_az1.id
 }
 
-# export the private data subnet az2 id
+// export the private data subnet az2 id
 output "private_data_subnet_az2_id" {
   value = aws_subnet.private_data_subnet_az2.id
 }
 
-# export the first availability zone
+// export the first availability zone
 output "availability_zone_1" {
   value = data.aws_availability_zones.available_zones.names[0]
 }
 
-# export the second availability zone
+// export the second availability zone
 output "availability_zone_2" {
   value = data.aws_availability_zones.available_zones.names[1]
 }
